c668685782fffdb96db455090cb2a525744cc7b2
Nikolay Bobkov <Zeal0t89@yandex.ru>
2020-11-06 23:59:16 +0300

    fix: доработка роутеров

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 
@@ -16,12 +16,12 @@ const NotFoundError = require('./errors/not-found-err');
 // const { login, createUser } = require('./controllers/users.js');
 
 const app = express();
-app.use(helmet());
+// app.use(helmet());
 
 const { PORT = 3000 } = process.env;
 
 const limit = rateLimit({
-  windowMs: 16 * 60 * 1000,
+  windowMs: 15 * 60 * 1000,
   max: 100,
   message: 'Слишком много запросов!',
 });
