774df3427039423070cae38ad5c9a10133483d01
Nikolay Bobkov <Zeal0t89@yandex.ru>
2020-11-06 15:54:28 +0300

    fix: добавил валидацию роутам

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,29 @@
 const articlesRouter = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const {
   getArticle, addArticle, deleteArticle,
 } = require('../controllers/articles');
 
 articlesRouter.get('/articles', getArticle);
-articlesRouter.post('/articles', addArticle);
-articlesRouter.delete('/articles/:articleId', deleteArticle);
+articlesRouter.post('/articles', celebrate({
+  body: Joi.object().keys({
+    keyword: Joi.string().required().min(2).max(30),
+    title: Joi.string().required().min(2).max(30),
+    text: Joi.string().required().min(2).max(30),
+    date: Joi.string().required().min(2).max(30),
+    source: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/,
+    ),
+    image: Joi.string().required().pattern(
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/,
+    ),
+  }),
+}), addArticle);
+articlesRouter.delete('/articles/:articleId', celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().alphanum().length(24).hex(),
+  }),
+}), deleteArticle);
 
 module.exports = { articlesRouter };
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,15 @@
 const usersRouter = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const { getUsers, getUser } = require('../controllers/users');
 
 usersRouter.get('/users',
   getUsers);
 
-usersRouter.get('/users/me',
-  getUser);
+usersRouter.get('/users/me', celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().alphanum().length(24).hex(),
+  }),
+}),
+getUser);
 
 module.exports = { usersRouter };
-// celebrate({
-//  params: Joi.object().keys({
-//    name: Joi.string().required().min(2).max(30),
-//    email: Joi.string().required().email(),
-//  }),
-// }),
